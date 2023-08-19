fec43c640a65d2b99e0e4522595b74e022cb8bde
Naman Khater <naman22khater@gmail.com>
2023-08-20 03:15:30 +0530

    Transaction form design added

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,14 +1,19 @@
+import { Container } from '@mui/system';
 import './App.css';
 import ResponsiveAppBar from './components/layout/header';
 
 import theme from './components/layout/theme';
 
 import { ThemeProvider } from '@mui/material/styles';
+import TransactionForm from './components/forms/Transactions';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <ResponsiveAppBar />
+      <Container>
+        <TransactionForm />
+      </Container>
     </ThemeProvider>
   );
 }
