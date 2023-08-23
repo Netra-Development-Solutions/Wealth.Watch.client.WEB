511dc062fca56c45d55bef828968ec5100509c28
Naman Khater <naman22khater@gmail.com>
2023-08-24 01:47:26 +0530

    UI upgrade with dummy pie chart

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -5,23 +5,31 @@ import ResponsiveAppBar from './components/layout/header';
 import theme from './components/layout/theme';
 import { ThemeProvider } from '@mui/material/styles';
 import TransactionForm from './components/forms/Transactions';
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, Grid } from '@mui/material';
 import CreditCardForm from './components/forms/CreditCard';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import HomePageComponent from './components/HomePageComponent';
 
 function App() {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
             <ResponsiveAppBar />
-            <Container maxWidth="md">
-                <BankAccountForm />
+
+            <Container>
+                <Paper sx={{ display: 'flex', flexDirection: 'column' }} elevation={1}>
+                    <Grid container>
+                        <Grid item xs={12} md={6}>
+                            <HomePageComponent />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <TransactionForm />
+                        </Grid>
+                    </Grid>
+                </Paper>
             </Container>
-            <Container maxWidth="md">
-                <CreditCardForm />
-            </Container>
-            <Container maxWidth="md">
-                 <TransactionForm />
-            </Container>
+
         </ThemeProvider>
     );
 }
