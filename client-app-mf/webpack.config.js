e9de62a4dc89e741ed567e82db1bf493a63ef9a8
Naman Khater <naman22khater@gmail.com>
2023-10-09 21:46:16 +0530

    initial

--- client-app-mf/webpack.config.js
+++ client-app-mf/webpack.config.js
@@ -1,14 +1,19 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const plugin_injector = require("./plugin-injector.json");
 
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3000/",
+    publicPath: "https://test-blob.s3.ap-south-1.amazonaws.com/plugins/client-app-mf/1.0.1/",
   },
 
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  optimization: {
+    chunkIds: "named",
   },
 
   devServer: {
@@ -43,11 +48,7 @@ module.exports = (_, argv) => ({
     new ModuleFederationPlugin({
       name: "client_app_mf",
       filename: "remoteEntry.js",
-      remotes: {
-        "wealth_watch_transaction_react": "wealth_watch_transaction_react@http://localhost:3002/remoteEntry.js",
-        "user_management": "user_management@http://localhost:3001/remoteEntry.js",
-        "account_management": "account_management@http://localhost:3003/remoteEntry.js",
-      },
+      remotes: plugin_injector,
       exposes: {},
       shared: {
         ...deps,
